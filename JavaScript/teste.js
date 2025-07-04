c8781c632e1f88ecd03773112fe4253d84bb6dc5
Ana Clara Lima Dantas de Medeiros <20231041110031@ifrn.local>
2025-07-04 08:05:00 -0300

    JavaScript

---
+++ JavaScript/teste.js
@@ -0,0 +1,25 @@
+//Mostrar uma mensagem, o print do JavaScript, console.log()
+
+// Criar  variáveis let e const
+
+//mudar conteúdo de uma variável let nomeDaVariável = ""
+
+//Comentários //Linha única e /*Várias linhas */
+let nome = "Ana Clara";
+nome = "Ana";
+const nome2 = "Angela";
+console.log(nome,",",nome2);
+if (nome === nome2){
+    console.log(`Olá ${nome}! Tenho ${idade} anos`)
+};
+function nomeDaFuncao(){
+    console.log("Olá, " + nome)
+};
+// Criando uma classe em JS 
+let funcionario = {
+    nome: "Pedro",
+    matricula: "20478",
+    idade: 23 , 
+    funcao: "nutricionista",   
+}
+console.log(`Usuário: \n ${funcionario.nome}`)
