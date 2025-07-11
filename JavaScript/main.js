cd5a13e6cc5274e5fc0427edd9902a8e2eb69f87
Ana Clara Lima Dantas de Medeiros <20231041110031@ifrn.local>
2025-07-11 13:13:34 -0300

    Novos aprendizados de JavaScript

---
+++ JavaScript/main.js
@@ -0,0 +1,28 @@
+//Aula de Tiago 10/07/2025 
+
+var variavel1;
+let variavel2;
+const variavel3 = "Ana";
+//ao invés de elif use else if
+
+//&& marcador de e
+
+//||marcador de ou
+
+const idade = "10";
+if(idade < 18){
+    console.log("Você não pode ter sucesso");
+}else if(idade>=18 && idade<25){
+    console.log("Você pode entrar com ressalvas...");
+}else{
+    console.log("Você pode entrar");
+};
+for(let i=0; i<10; i++);
+
+    const pessoas = {
+        endereço:{
+            rua:"rua lallala",
+        }
+    }
+
+    console.log(pessoas.endereço.rua);
